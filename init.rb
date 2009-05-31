0adba535571b671926e512a703c24bcced31e79d
burt <funkingburt@gmail.com>
2009-05-31 16:01:15 +0100

    query locals

--- init.rb
+++ init.rb
@@ -2,3 +2,4 @@ require 'gin'
 require 'extensions'
 require 'action_view'
 ActionView::Base.send :include, Gin::InstanceMethods
+# ActionView::Template.register_template_handler :gin, Gin::TemplateHandler
--- lib/gin.rb
+++ lib/gin.rb
@@ -2,7 +2,35 @@ require 'action_controller'
 
 module Gin
   
+  class TagHelper
+    
+    include ActionView::Helpers::JavaScriptHelper
+    include ActionView::Helpers::TagHelper
+    
+    def self.format_key(key)
+      key.to_s.camelize(:lower)
+    end
+    
+    module Formatter
+      
+      def format_key(key)
+        Gin::TagHelper.format_key(key)
+      end
+      
+      def format_locals(locals = {})
+        vars_string = ""
+        locals.each_pair do |key, value|
+          vars_string << "var #{format_key(key)} = #{value.to_gin};\n"
+        end
+        vars_string
+      end
+      
+    end
+    
+  end
+  
   module InstanceMethods
+    include Gin::TagHelper::Formatter
     
     def jquery_ready_script(&block)
       Gin::Tags::DomReady.new(self, &block).to_s
@@ -13,15 +41,8 @@ module Gin
       concat javascript_tag(content)
     end
     
-  end
-  
-  class TagHelper
-    
-    include ActionView::Helpers::JavaScriptHelper
-    include ActionView::Helpers::TagHelper
-    
-    def self.format_key(key)
-      key.to_s.camelize(:lower)
+    def jquery_locals(locals ={})
+      format_locals locals
     end
     
   end
@@ -29,6 +50,7 @@ module Gin
   module Tags
     
     class Tag
+      include Gin::TagHelper::Formatter
       
       attr_reader :parent, :content
       
@@ -41,12 +63,6 @@ module Gin
   		def <<(val)
   			@content << val
   		end
-      
-      protected
-      
-      def format_key(key)
-        Gin::TagHelper.format_key(key)
-      end
       
   	end
 
@@ -108,5 +124,44 @@ module Gin
 	  end
     
   end
+
+=begin
+  class TemplateHandler < ActionView::TemplateHandler
+
+    def render(template, locals)
+      # @view.controller.headers["Content-Type"] = 'text/javascript; charset=utf-8'
+      
+      template = if template.is_a? ActionView::Base
+        template.template
+      elsif template.is_a? ActionView::ReloadableTemplate
+        template
+      end
+
+      # remove any unwanted guff
+      locals.delete (template.name[1, template.name.size].to_sym)
+      locals.delete :object
+      "#{format_locals(locals)}\n#{template.source}"
+    end
+      
+    def compilable?
+      false
+    end
+    
+    private
+    
+    def format_locals(locals)
+      vars_string = ""
+      locals.each_pair do |key, value|
+        vars_string << "var #{format_key(key)} = #{value.to_gin};\n"
+      end
+      vars_string
+    end
+    
+    def format_key(key)
+      Gin::TagHelper.format_key(key)
+    end
+
+  end
+=end
   
 end
